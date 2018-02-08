5ffde65330b0f5febcbc3190b9b5f4569cc768d1
Lukáš Salich <lukas.salich@betsys.com>
2018-02-08 19:28:29 +0100

    implemented searching for line+circle intersections

--- convex_hull_of_intersections.cpp
+++ convex_hull_of_intersections.cpp
@@ -4,10 +4,11 @@
 #include <fstream>
 #include <cmath>
 #include <iomanip>
+#include <set>
 
 using namespace std;
 
-struct square_area {
+struct square_area { // in this task, I use "line" instead of "line segment", because it's shorter
     int lower_left_x, lower_left_y, top_right_x, top_right_y;
     int first_x, first_y, second_x, second_y;
     int center_x, center_y, radius;
@@ -28,7 +29,7 @@ int main() {
     //cin >> element_count;
     //cin.sync();
     vector<square_area> elements(element_count);
-    vector<pair<double, double>> collisions;
+    set<pair<double, double>> collisions; // TODO try other containers
     for (auto& element : elements) {
         string line;
         //getline(cin, line);
@@ -60,7 +61,7 @@ int main() {
         for (auto& element_to_check_intersection : elements) {
             if (&element_to_check_intersection == &element)
                 break;
-            if (((element.lower_left_x >= element_to_check_intersection.lower_left_x && element.lower_left_x <= element_to_check_intersection.top_right_x) ||
+            if (((element.lower_left_x >= element_to_check_intersection.lower_left_x && element.lower_left_x <= element_to_check_intersection.top_right_x) || // TODO try to remove
                 (element_to_check_intersection.lower_left_x >= element.lower_left_x && element_to_check_intersection.lower_left_x <= element.top_right_x))
                 && ((element.lower_left_y >= element_to_check_intersection.lower_left_y && element.lower_left_y <= element_to_check_intersection.top_right_y) ||
                 (element_to_check_intersection.lower_left_y >= element.lower_left_y && element_to_check_intersection.lower_left_y <= element.top_right_y)))
@@ -78,28 +79,65 @@ int main() {
                     double s = (element_square_x_length * starting_y_difference - element_square_y_length * starting_x_difference) / r;
                     if (s < 0 || s > 1)
                         continue;
+
                     double t = (element_to_check_intersection_square_x_length * starting_y_difference - element_to_check_intersection_square_y_length * starting_x_difference) / r;
                     if (t < 0 || t > 1)
                         continue;
-                    collisions.emplace_back(element.first_x + (t * element_square_x_length), element.first_y + (t * element_square_y_length));
+
+                    collisions.emplace(element.first_x + (t * element_square_x_length), element.first_y + (t * element_square_y_length));
                 }
                 else if (element.type == square_area::square_area_circle && element_to_check_intersection.type == square_area::square_area_circle) { // TODO caching
-                    double distance_between_centers = hypot((element_to_check_intersection.center_x - element.center_x), (element_to_check_intersection.center_y - element.center_y));
+                    double distance_between_centers_x = element_to_check_intersection.center_x - element.center_x;
+                    double distance_between_centers_y = element_to_check_intersection.center_y - element.center_y;
+                    double distance_between_centers = hypot(distance_between_centers_x, distance_between_centers_y);
                     if (distance_between_centers > element.radius + element_to_check_intersection.radius || distance_between_centers < abs(element.radius - element_to_check_intersection.radius))
                         continue;
-                    double a = (element.radius * element.radius - element_to_check_intersection.radius * element_to_check_intersection.radius + distance_between_centers * distance_between_centers) / (2 * distance_between_centers);
-                    double h = sqrt(element.radius * element.radius - a * a);
-                    double helper1 = (element_to_check_intersection.center_x - element.center_x) / distance_between_centers;
-                    double helper2 = (element_to_check_intersection.center_y - element.center_y) / distance_between_centers;
+
+                    double element_radius_square = element.radius * element.radius;
+                    double a = (element_radius_square - element_to_check_intersection.radius * element_to_check_intersection.radius + distance_between_centers * distance_between_centers) / (2 * distance_between_centers);
+                    double h = sqrt(element_radius_square - a * a);
+
+                    double helper1 = distance_between_centers_x / distance_between_centers;
+                    double helper2 = distance_between_centers_y / distance_between_centers;
                     double helper3 = h * helper1;
                     double helper4 = h * helper2;
+
                     double intersection_area_center_x = element.center_x + a * helper1;
                     double intersection_area_center_y = element.center_y + a * helper2;
-                    collisions.emplace_back(intersection_area_center_x + helper4,  intersection_area_center_y - helper3);
-                    collisions.emplace_back(intersection_area_center_x - helper4,  intersection_area_center_y + helper3);
+
+                    collisions.emplace(intersection_area_center_x + helper4,  intersection_area_center_y - helper3);
+                    collisions.emplace(intersection_area_center_x - helper4,  intersection_area_center_y + helper3);
                 }
                 else {
-                    ;
+                    square_area& line = element.type == square_area::square_area_line ? element : element_to_check_intersection;
+                    square_area& circle = element.type == square_area::square_area_circle ? element : element_to_check_intersection;
+                    
+                    int line_x_length = line.second_x - line.first_x;
+                    int line_y_length = line.second_y - line.first_y;
+
+                    int distance_between_circle_center_and_line_first_x = circle.center_x - line.first_x;
+                    int distance_between_circle_center_and_line_first_y = circle.center_y - line.first_y;
+                    
+                    double line_length = hypot(line_x_length, line_y_length);
+                    double distance_between_center_and_line = abs(line_x_length * distance_between_circle_center_and_line_first_y - distance_between_circle_center_and_line_first_x * line_y_length) / line_length;
+                    if (distance_between_center_and_line > circle.radius)
+                        continue;
+                    
+                    double Dx = line_x_length / line_length;
+                    double Dy = line_y_length / line_length;
+                    
+                    double distance_from_A_to_closest_point_to_circle_center_on_line = Dx * distance_between_circle_center_and_line_first_x + Dy * distance_between_circle_center_and_line_first_y;
+                    double distance_from_circle_center_to_line = sqrt(circle.radius * circle.radius - distance_between_center_and_line * distance_between_center_and_line);
+                    
+                    double helper1 = distance_from_A_to_closest_point_to_circle_center_on_line - distance_from_circle_center_to_line;
+                    if (helper1 / line_length >= 0 && helper1 / line_length <= 1) // TODO optimize "helper1 / line_length"
+                        collisions.emplace(line.first_x + helper1 * Dx,  line.first_y + helper1 * Dy);
+
+                    double helper2 = distance_from_A_to_closest_point_to_circle_center_on_line + distance_from_circle_center_to_line;
+                    if (distance_between_center_and_line != circle.radius) {
+                        if (helper2 / line_length >= 0 && helper2 / line_length <= 1) // TODO optimize "helper2 / line_length"
+                            collisions.emplace(line.first_x + helper2 * Dx,  line.first_y + helper2 * Dy);
+                    }
                 }
             }
         }
